0411352ece27e714f34f4dd68b505cf53f8a4081
lfenart <lucas.fenart@protonmail.com>
2020-06-22 18:03:51 +0200

    Fix rust example

--- example/rust/src/main.rs
+++ example/rust/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{LineWriter, BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader, LineWriter, Write};
 use std::process::{Command, Stdio};
 
 const PATH: &'static str = "./rubot-holes";
@@ -23,10 +23,6 @@ fn main() {
     let mut stdin = LineWriter::new(rubot.stdin.unwrap());
     let mut stdout = BufReader::new(rubot.stdout.unwrap());
 
-    // reply: "nok"
-    stdin.write(b"isready\n").unwrap();
-    let _ = read(&mut stdout);
-
     // sets a new game, first blocks are I, J, L, O, S, T, Z
     stdin.write(b"newgame IJLOSTZ\n").unwrap();
 
@@ -34,22 +30,37 @@ fn main() {
     stdin.write(b"isready\n").unwrap();
     let _ = read(&mut stdout);
 
-    // start the game
+    // start searching and returns the best move
     stdin.write(b"go\n").unwrap();
+    let best_move = read(&mut stdout);
+
+    // play the best move
+    let mut play = String::from("play ");
+    play.push_str(&best_move);
+    stdin.write(play.as_bytes()).unwrap();
+
+    // play move hold = false, rotation = 1, translation = 2, spin = 3
+    stdin.write(b"play 0 1 2 3\n").unwrap();
+
+    // add handicap line with a hole (or bomb) in column 0
+    stdin.write(b"handicap 0\n").unwrap();
+
+    // add handicap lines with a hold in columns 1 (top) and 2 (bottom)
+    stdin.write(b"handicap 1 2\n").unwrap();
+
+    // add block I
+    stdin.write(b"block I\n").unwrap();
+
+    // add blocks O and S
+    stdin.write(b"block O S\n").unwrap();
+
+    // ko, removes all handicap lines
+    stdin.write(b"ko\n").unwrap();
+
+    // print the state of the robot
+    stdin.write(b"print\n").unwrap();
     let _ = read(&mut stdout);
 
-    // no ko, next block is I, 3 handicap lines added on columns 3, 4 and 5
-    stdin.write(b"0 I 3 4 5\n").unwrap();
-    let _ = read(&mut stdout);
-
-    // ko, next block is O, no handicap line added
-    stdin.write(b"1 O\n").unwrap();
-    let _ = read(&mut stdout);
-
-    // quit the game
-    stdin.write(b"q\n").unwrap();
-    let _ = read(&mut stdout);
-    
     // exit the processus
     stdin.write(b"exit\n").unwrap();
 }
